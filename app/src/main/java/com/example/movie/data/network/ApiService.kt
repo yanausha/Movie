59bace473e996747e4c4dfe392b12cae5a453ac2
Yana Ushakova <junk7day@gmail.com>
2023-01-24 14:23:28 +0300

    add api service

---
+++ app/src/main/java/com/example/movie/data/network/ApiService.kt
@@ -0,0 +1,13 @@
+package com.example.movie.data.network
+
+import com.example.movie.data.network.model.MovieResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("movie?token=XMHZ37R-C9RMRAY-PKSH04M-8RW8FFE&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=10")
+    suspend fun getMovies(
+        @Query("page") page: Int
+    ): MovieResponseDto
+}
