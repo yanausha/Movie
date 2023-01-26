06e10db26596d97a7fb2aa739c979e69e5b31f0c
Yana Ushakova <junk7day@gmail.com>
2023-01-26 12:09:40 +0300

    set background for rating and poster

--- app/src/main/java/com/example/movie/presentation/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/movie/presentation/adapter/MovieAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.bumptech.glide.Glide
+import com.example.movie.R
 import com.example.movie.data.network.model.MovieDto
 import com.example.movie.databinding.MovieItemBinding
 
@@ -23,11 +24,25 @@ class MovieAdapter : ListAdapter<MovieDto, MovieViewHolder>(MovieDiffCallback) {
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
 
         val movie = getItem(position)
-        Glide.with(holder.itemView)
-            .load(movie.poster.url)
-            .into(holder.binding.imageViewPoster)
+        val rating = movie.rating.kp ?: 0.00
+
+        try {
+            Glide.with(holder.itemView)
+                .load(movie.poster.url)
+                .into(holder.binding.imageViewPoster)
+        } catch (e: Exception) {
+            holder.binding.imageViewPoster.setBackgroundResource(R.drawable.noposter)
+        }
 
         holder.binding.textViewRating.text = String.format("%.2f", movie.rating.kp)
+
+        with(holder.binding) {
+            when (rating) {
+                in 7.5..10.0 -> textViewRating.setBackgroundResource(R.drawable.circle_super)
+                in 5.5..7.5 -> textViewRating.setBackgroundResource(R.drawable.circle_good)
+                else -> textViewRating.setBackgroundResource(R.drawable.circle_bad)
+            }
+        }
 
         if (position >= this.currentList.size - 10) onScrollListener?.onScrollListEnd()
     }
