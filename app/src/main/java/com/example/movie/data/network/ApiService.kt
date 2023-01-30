295c3b94acef1dd8715399dc65ba7b10f8a5cfb8
Yana Ushakova <junk7day@gmail.com>
2023-01-30 11:37:28 +0300

    edit token

--- app/src/main/java/com/example/movie/data/network/ApiService.kt
+++ app/src/main/java/com/example/movie/data/network/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("movie?token=XMHZ37R-C9RMRAY-PKSH04M-8RW8FFE&field=rating.kp&search=5-10&sortField=votes.kp&sortType=-1&limit=20")
+    @GET("movie?token=2EA18DT-MHPMKV6-HJEPKT6-3G6BTKJ&field=rating.kp&search=5-10&sortField=votes.kp&sortType=-1&limit=20")
     suspend fun getMovies(
         @Query("page") page: Int
     ): MovieResponseDto
